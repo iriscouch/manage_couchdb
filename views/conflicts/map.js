b5641476b6dc8d8663013f4021d263bcceb2386d
Jason Smith <jhs@iriscouch.com>
2011-05-09 04:07:30 +0700

    Emit one row for each conflicted revision

--- views/conflicts/map.js
+++ views/conflicts/map.js
@@ -1,8 +1,18 @@
 function(doc) {
-  if(doc._conflicts) doc._conflicts.forEach(function(conflicted_revision) {
-    var key = [doc._id, conflicted_revision];
-    var value = 1; // For counting conflicts`
+  if(doc._conflicts) {
 
-    emit(key, value);
-  })
+    function emit_rev(rev) {
+      var value = 1; // The fast built-in _sum reduction will result in a count of conflicted revisions.
+      var key = [doc._id, rev];
+
+      emit(key, value);
+    }
+
+    // First emit the "winning" revision.
+    emit_rev(doc._rev);
+
+    // Next emit all the revisions properly recorded as a conflict.
+    for(var a = 0; a < doc._conflicts.length; a++)
+      emit_rev(doc._conflicts[a]);
+  }
 }
