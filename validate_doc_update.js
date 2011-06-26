9dd16d143d06708c73d89268e260400dede419cc
Jason Smith (workstation) <jhs@iriscouch.com>
2011-06-26 10:36:30 +0000

    Permit the DB admin to change a read-only DB

--- validate_doc_update.js
+++ validate_doc_update.js
@@ -1,6 +1,24 @@
 function(newDoc, oldDoc, userCtx, secObj) {
   var ddoc = this;
 
+  secObj.admins = secObj.admins || {};
+  secObj.admins.names = secObj.admins.names || [];
+  secObj.admins.roles = secObj.admins.roles || [];
+
+  var IS_DB_ADMIN = false;
+  var i;
+
+  if(userCtx.roles.indexOf('_admin') !== -1)
+    IS_DB_ADMIN = true;
+  if(secObj.admins.names.indexOf(userCtx.name) !== -1)
+    IS_DB_ADMIN = true;
+  for(i = 0; i < userCtx.roles; i++)
+    if(secObj.admins.roles.indexOf(userCtx.roles[i]) !== -1)
+      IS_DB_ADMIN = true;
+
   if(ddoc.access && ddoc.access.read_only)
-    throw {forbidden: "This database is read-only"};
+    if(IS_DB_ADMIN)
+      log('Admin change on read-only db: ' + newDoc._id);
+    else
+      throw {forbidden: "This database is read-only"};
 }
