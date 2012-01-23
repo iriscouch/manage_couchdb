af8c4dc53df2cd80be4d1e4b3dfcaa813daad596
Jason Smith (work) <jhs@iriscouch.com>
2012-01-23 05:37:53 +0000

    Tighten up the view a bit

--- views/conflicts/map.js
+++ views/conflicts/map.js
@@ -1,20 +1,18 @@
 function(doc) {
   if(doc._conflicts) {
-
-    function emit_rev(rev) {
-      var key = [doc._id, rev];
-
-      // Create a value that will pull the correct revision when ?include_docs=true is specified.
-      var value = { "_id": doc._id, "_rev": rev };
-
-      emit(key, value);
-    }
-
     // First emit the "winning" revision.
-    emit_rev(doc._rev);
+    emit_rev(doc._rev)
 
     // Next emit all the revisions properly recorded as a conflict.
     for(var a = 0; a < doc._conflicts.length; a++)
-      emit_rev(doc._conflicts[a]);
+      emit_rev(doc._conflicts[a])
+  }
+
+  function emit_rev(rev) {
+    // Emit a value that will pull the correct revision for ?include_docs=true.
+    var key = [doc._id, rev]
+      , value = { "_id": doc._id, "_rev": rev }
+
+    emit(key, value)
   }
 }
