47cc4f5eb6af37f81c1ee264e4bfd26754b72376
Jason Smith (air) <jhs@iriscouch.com>
2012-03-19 07:27:20 +0700

    Tighten the code and style

--- validate_doc_update.js
+++ validate_doc_update.js
@@ -6,19 +6,17 @@ function(newDoc, oldDoc, userCtx, secObj) {
   secObj.admins.roles = secObj.admins.roles || [];
 
   var IS_DB_ADMIN = false;
-  var i;
-
-  if(userCtx.roles.indexOf('_admin') !== -1)
+  if(~ userCtx.roles.indexOf('_admin'))
     IS_DB_ADMIN = true;
-  if(secObj.admins.names.indexOf(userCtx.name) !== -1)
+  if(~ secObj.admins.names.indexOf(userCtx.name))
     IS_DB_ADMIN = true;
-  for(i = 0; i < userCtx.roles; i++)
-    if(secObj.admins.roles.indexOf(userCtx.roles[i]) !== -1)
+  for(var i = 0; i < userCtx.roles; i++)
+    if(~ secObj.admins.roles.indexOf(userCtx.roles[i]))
       IS_DB_ADMIN = true;
 
   if(ddoc.access && ddoc.access.read_only)
     if(IS_DB_ADMIN)
       log('Admin change on read-only db: ' + newDoc._id);
     else
-      throw {forbidden: "This database is read-only"};
+      throw {'forbidden':'This database is read-only'};
 }
