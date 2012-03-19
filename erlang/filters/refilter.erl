d8579d5f2c272103f1cd4e9a344018ff790fce54
Jason Smith (work) <jhs@iriscouch.com>
2012-03-19 09:28:14 +0000

    Boost refilter's performance by commenting out all logging and generation of the logger function

--- erlang/filters/refilter.erl
+++ erlang/filters/refilter.erl
@@ -7,40 +7,45 @@
 % ]}
 
 fun({Doc}, {Req}) -> ok
-    , Id = couch_util:get_value(<<"_id">>, Doc)
     , {Query} = couch_util:get_value(<<"query">>, Req)
-    , L = case couch_util:get_value(<<"log">>, Query)
-        of true      -> fun(F, A) -> Log(lists:flatten(io_lib:format(F, A))) end
-        ; <<"true">> -> fun(F, A) -> Log(lists:flatten(io_lib:format(F, A))) end
-        ; _          -> fun(_F, _A) -> ok end
-        end
+
+    % Unfortunately, building a function for every execution (i.e. once per doc) imposes a steep
+    % performance penalty. I tried the process dictionary to memoize it but that didn't help.
+    % So, uncomment this to activate logging. In the Javascript version, performance is secondary,
+    % so it should support logging by default.
+
+    %, Logger = case couch_util:get_value(<<"log">>, Query)
+    %    of true      -> fun(F, A) -> Log(lists:flatten(io_lib:format(F, A))) end
+    %    ; <<"true">> -> fun(F, A) -> Log(lists:flatten(io_lib:format(F, A))) end
+    %    ; _          -> fun(_F, _A) -> ok end
+    %    end
 
     , Field = couch_util:get_value(<<"field">>, Query)
     , Regex = couch_util:get_value(<<"regex">>, Query)
-    , L("Running: Id=~p\nReq=~p", [Id, Req])
+    %, Id = couch_util:get_value(<<"_id">>, Doc)
+    %, Logger("Running: Id=~p\nReq=~p", [Id, Req])
 
     , case is_binary(Field) andalso is_binary(Regex)
         of false -> ok
-            , L("Invalid parameters: Field=~p Regex=~p", [Field, Regex])
+            %, Logger("Invalid parameters: Field=~p Regex=~p", [Field, Regex])
             , exit({invalid_query, <<"Required field and regex parameter">>})
         ; true -> ok
-            , L("Field=~p Regex=~p", [Field, Regex])
+            %, Logger("Field=~p Regex=~p", [Field, Regex])
             , case couch_util:get_value(Field, Doc)
                 of undefined -> ok
-                    , L("Doc ~s has no field ~s", [Id, Field])
+                    %, Logger("Doc ~s has no field ~s", [Id, Field])
                     , false
                 ; Non_bin when not is_binary(Non_bin) -> ok
-                    , L("Doc ~s field ~s is not a string: ~p", [Id, Field, Non_bin])
+                    %, Logger("Doc ~s field ~s is not a string: ~p", [Id, Field, Non_bin])
                     , false
                 ; Value -> ok
-                    , L("Testing '~s' to '~s'", [Value, Regex])
-                    , {ok, Pattern} = re:compile(Regex)
-                    , case re:run(Value, Pattern, [])
+                    %, Logger("Testing '~s' to '~s'", [Value, Regex])
+                    , case re:run(Value, Regex, [])
                         of nomatch -> ok
-                            , L("No match: ~s", [Id])
+                            %, Logger("No match: ~s", [Id])
                             , false
                         ; _ -> ok
-                            , L("Match: ~s", [Id])
+                            %, Logger("Match: ~s", [Id])
                             , true
                         end
                 end
