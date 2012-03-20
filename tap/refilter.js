8cc3c379551c9ba0dce16e65ebdbc3dd582f4688
Jason Smith (air) <jhs@iriscouch.com>
2012-03-20 09:02:17 +0700

    Just 100 docs to speed up testing

--- tap/refilter.js
+++ tap/refilter.js
@@ -13,7 +13,7 @@ test('Refilter', function(t, type) {
 
   var rnd = Math.random().toString().replace(/^0\./, '')
   var docs = []
-  for(var i = 1; i <= 1000; i++)
+  for(var i = 1; i <= 100; i++)
     docs.push({'type':type, 'run':[type, rnd, i].join('_')})
 
   t.plan(5)
@@ -36,19 +36,19 @@ test('Refilter', function(t, type) {
         feed += '&regex=^' + type + '_' + rnd
         request({'url':feed, 'json':true}, function(er, res) {
           if(er) throw er
-          t.equal(res.body.results.length, 1000, 'Got 1000 documents through the filter')
+          t.equal(res.body.results.length, 100, 'Got 100 documents through the filter')
 
           var ones = feed + '_1'
             , twos = feed + '_(2|87$)'
 
           request({'url':ones, 'json':true}, function(er, res) {
             if(er) throw er
-            var ones_count = 1 + 10 + 100 + 1 // 1, 10-19, 100-199, 1000
+            var ones_count = 1 + 10 + 1 // 1, 10-19, 100
             t.equal(res.body.results.length, ones_count, 'Got all docs with i starting with 1')
 
             request({'url':twos, 'json':true}, function(er, res) {
               if(er) throw er
-              var twos_count = 1 + 10 + 100 + 1 // 2, 20-29, 200-299, 87
+              var twos_count = 1 + 10 + 1 // 2, 20-29, 87
               //console.dir(res.body.results)
               t.equal(res.body.results.length, twos_count, 'Got all docs with i starting with 2, or i == 87')
 
